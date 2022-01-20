5a757639d4ef3b7c21189be960da8a979f1dde48
Kvazar3S273 <ba2h007@gmail.com>
2022-01-21 00:22:41 +0200

    try to generate db

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -1,10 +1,9 @@
 package program;
 
+import com.github.javafaker.Faker;
+
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -18,7 +17,8 @@ public class Main {
 
     public static void main(String[] args) {
         String strConn = "jdbc:mariadb://localhost:3306/javadb";
-        InsertIntoDB(strConn);
+        //InsertIntoDB(strConn);
+        //GenerateProducts(strConn);
         List<Product> list = SelectFromDB(strConn);
         PrintProductList(list);
         //UpdateForDB(strConn);
@@ -31,6 +31,53 @@ public class Main {
         for (Product p : prods) {
             System.out.println(p.toString());
         }
+    }
+
+    public static boolean isTableExist(String strConn) {
+        boolean exist = false;
+        try (Connection con = DriverManager.getConnection(strConn, "root", "");
+             PreparedStatement preparedStatement = con.prepareStatement("SELECT * FROM products");) {
+            try (ResultSet resultSet = preparedStatement.executeQuery();) {
+                if (resultSet.next()) {
+                    exist = true;
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return exist;
+    }
+
+    private static final String insertFakerSQL = " INSERT INTO products " +
+            " (id,name,price,description) VALUES " + "(?,?,?,?);";
+
+    private static void GenerateProducts(String strConn) {
+        Faker faker = Faker.instance(new Locale("uk"));
+        try (Connection con = DriverManager.getConnection(strConn, "root", "");
+
+             PreparedStatement preparedStatement = con.prepareStatement(insertFakerSQL);) {
+
+            if (!isTableExist(strConn)) {
+                List<Product> generatedProdList = new ArrayList<>();
+                for (int i = 0; i < 10; i++) {
+                    preparedStatement.setInt(1, faker.random().nextInt(0, 10));
+                    preparedStatement.setString(2, faker.commerce().productName());
+                    preparedStatement.setDouble(3, faker.random().nextDouble());
+                    preparedStatement.setString(4, faker.commerce().color());
+                    preparedStatement.addBatch();
+                }
+                System.out.println("База згенерована");
+            } else {
+                System.out.println("Ви не можете згенерувати базу, так як вона вже згенерована!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error connection: " + ex.getMessage());
+        }
+
+
     }
 
     private static void InsertIntoDB(String strConn) {
